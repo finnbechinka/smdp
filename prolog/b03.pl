9240b7d7f58217b234e59f58f608c8ddac86255d
Finn <contact@bechinka.eu>
2023-05-04 13:44:31 +0200

    feat: Hauptstädte, Länder, Kontinente

--- prolog/b03.pl
+++ prolog/b03.pl
@@ -1,3 +1,4 @@
+% Wissensbasis erstellen
 buch(buch1, author1, drama).
 buch(buch2, author1, krimi).
 buch(buch3, author1, comedy).
@@ -9,3 +10,23 @@ buch(buch7, author2, krimi).
 
 % buch(X, A, _).
 % buch(X, A, G).
+
+
+% Hauptstädte, Länder, Kontinente
+
+istHauptstadtVon(berlin, deutschland).
+istHauptstadtVon(paris, frankreich).
+istHauptstadtVon(peking, china).
+
+liegtImKontinent(deutschland, europa).
+liegtImKontinent(frankreich, europa).
+liegtImKontinent(china, asien).
+
+stadtLiegtInKontinent(S, K) :- istHauptstadtVon(S, L), liegtImKontinent(L, K).
+
+stadtLiegtInEuropa(S) :- stadtLiegtInKontinent(S, europa).
+stadtLiegtInAsien(S) :- stadtLiegtInKontinent(S, asien).
+
+liegenImGleichenKontinent(S1, S2) :- stadtLiegtInKontinent(S1, K), stadtLiegtInKontinent(S2, K).
+
+
