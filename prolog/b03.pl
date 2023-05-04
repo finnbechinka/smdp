f31e604a134a8264e3261133bddbb2b535eba4fc
Finn <contact@bechinka.eu>
2023-05-04 13:32:32 +0200

    feat: Wissensbasis erstellen

---
+++ prolog/b03.pl
@@ -0,0 +1,11 @@
+buch(buch1, author1, drama).
+buch(buch2, author1, krimi).
+buch(buch3, author1, comedy).
+
+buch(buch4, author2, drama).
+buch(buch5, author2, krimi).
+buch(buch6, author2, comedy).
+buch(buch7, author2, krimi).
+
+% buch(X, A, _).
+% buch(X, A, G).
