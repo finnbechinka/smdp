9ea29ea16699ec3d77c38cbe9e8ee3ab479312ff
Finn <contact@bechinka.eu>
2023-05-10 00:18:23 +0200

    feat: Klötzchenwelt (5 / 6)

--- prolog/b04.pl
+++ prolog/b04.pl
@@ -1,3 +1,5 @@
+% Klötzchenwelt
+
 on(a,b).
 on(b,table).
 on(c,d).
@@ -12,3 +14,11 @@ top(X) :- on(X, _), \+ on(_, X).
 subtower(El, T) :-
   on(El, X), subtower(X, T2), T = [El | T2];
   \+ on(El, _), T = [].
+
+
+tower(T) :- [TH|_] = T, top(TH), subtower(TH, T2), T = T2.
+
+higher([_|_], []) :- true.
+higher([_|T1T], [_|T2T]) :- higher(T1T, T2T).
+
+highest(H) :- tower(T), T \= H, higher(H, T).
