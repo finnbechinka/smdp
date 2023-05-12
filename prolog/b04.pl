421444e345958b71cf3368474a48aa7ea1e5a019
Finn <contact@bechinka.eu>
2023-05-12 17:36:35 +0200

    feat: Noch mehr Listen; us, addone, max

--- prolog/b04.pl
+++ prolog/b04.pl
@@ -22,3 +22,31 @@ higher([_|_], []) :- true.
 higher([_|T1T], [_|T2T]) :- higher(T1T, T2T).
 
 highest(H) :- tower(T), T \= H, higher(H, T).
+
+
+% Noch mehr Listen
+
+sum_ac([], X, R) :- X = R.
+sum_ac([L|T], X, R) :- X2 is R + L, sum_ac(T, X, X2).
+
+sum(L, X) :- sum_ac(L, X2, 0), X = X2.
+
+
+addone([], []).
+addone([A|TA], [B|TB]) :- B is A + 1, addone(TA, TB).
+
+combine([], [], X, Y) :- X = Y.
+combine([], [L2|T2], X, Y) :- combine([], T2, [L2|X], Y).
+combine([L1|T1], [L2|T2], X, Y) :- combine(T1, [L2|T2], [L1|X], Y).
+
+
+% permutation
+
+
+contains([L|T], X) :- L = X; contains(T, X).
+
+max_r([], _) .
+max_r([L|T], Z) :-
+  Z >= L, max_r(T, Z);
+  fail.
+max(L, Z) :- contains(L, Z), max_r(L, Z).
